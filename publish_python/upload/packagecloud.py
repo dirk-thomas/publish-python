57fa88245df0deda038c056b2adb19a71e415676
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-02-21 23:01:17 +0100

    Support for uploading deb source packages to packagecloud

--- publish_python/upload/packagecloud.py
+++ publish_python/upload/packagecloud.py
@@ -24,7 +24,7 @@ def upload_packagecloud(*, artifacts, upload, config):
         destination = f'{repository}{version}'
         for artifact in artifacts:
             ext = os.path.splitext(artifact)[1]
-            if ext not in ('.deb', '.whl'):
+            if ext not in ('.deb', '.whl', '.dsc'):
                 continue
 
             basename = os.path.basename(artifact)
